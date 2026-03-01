db6d0ec013a63ff96dc1d63863be8dca68ba44ec
sharadvyas123 <sharadvyas132@gmail.com>
2026-03-01 14:23:46 +0530

    frontend and backend created

--- backend/run.py
+++ backend/run.py
@@ -2,7 +2,7 @@ from app.agent.graph import agent
 from app.agent.state import SarthiState
 
 initial_state = SarthiState(
-    question="What is Karma Yoga? "
+    question="What is Karma Yoga?"
 )
 
 final_state = agent.invoke(initial_state)
